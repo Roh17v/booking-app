6267dedd7c45e8cad967fb3e9cc8efd5988830b8
Rohit Verma <Rohit.verma272727@gmail.com>
2024-08-21 06:09:38 +0000

    feat: Added Hotel model

---
+++ backend/models/hotel.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const hotelSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  photos: {
+    type: [String],
+  },
+  ratings: {
+    type: Number,
+    min: 0,
+    max: 5,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  distance: {
+    type: String,
+    required: true,
+  },
+  rooms: {
+    type: [String]
+  },
+  cheapestPrice: {
+    type: Number
+  },
+  featured: {
+    type: Boolean,
+    default: false
+  }
+});
+
+export const Hotel = mongoose.model("Hotel", hotelSchema);
