e81d7ab64092e7d983220cae2da0bb182ecc06f5
Rohit Verma <Rohit.verma272727@gmail.com>
2024-08-22 05:50:59 +0000

    refactor: Moved handlers to controllers directory

---
+++ backend/controllers/hotel.controller.js
@@ -0,0 +1,59 @@
+import { Hotel } from "../models/hotel.model.js";
+import { validateHotel } from "../models/hotel.model.js";
+
+export const createHotel = async (req, res) => {
+  const { error } = validateHotel(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  try {
+    const newHotel = new Hotel(req.body);
+    const result = await newHotel.save();
+    return res.status(201).send(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateHotel = async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    return res.status(200).send(updatedHotel);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteHotel = async (req, res) => {
+  try {
+    const deletedHotel = await Hotel.findByIdAndDelete(req.params.id);
+    if (!deletedHotel)
+      return res.status(404).send({ message: "Hotel Not Found!" });
+    return res.status(200).send(deletedHotel);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getHotel = async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    if (!hotel) return res.status(404).send({ message: "Hotel Not Found!" });
+    return res.status(200).send(hotel);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getHotels = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.find();
+    if (!hotel) return res.status(404).send({ message: "Hotel Not Found!" });
+    return res.status(200).send(hotel);
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/routes/hotel.js
+++ backend/routes/hotel.js
@@ -1,68 +1,27 @@
 import express from "express";
-import { Hotel, validateHotel } from "../models/hotel.model.js";
-import { createError } from "../utils/error.js";
+import {
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.controller.js";
 
 const router = express.Router();
 
 //CREATE
-router.post("/", async (req, res) => {
-  const { error } = validateHotel(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  try {
-    const newHotel = new Hotel(req.body);
-    const result = await newHotel.save();
-    return res.status(201).send(result);
-  } catch (error) {
-    console.log("Failed to Create the Hotel: ", error);
-    res.status(500).send(error);
-  }
-});
+router.post("/", createHotel);
 
 //UPDATE
-router.put("/:id", async (req, res) => {
-    try {
-      const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id,{$set: req.body},{new: true});
-      return res.status(200).send(updatedHotel);
-    } catch (error) {
-      console.log("Failed to Update the Hotel: ", error);
-      res.status(500).send(error);
-    }
-  });   
+router.put("/:id", updateHotel);
 
 //DELETE
-router.delete("/:id", async (req, res) => {
-    try {
-      const deletedHotel = await Hotel.findByIdAndDelete(req.params.id);
-      if(!deletedHotel) return res.status(404).send({message: "Hotel Not Found!"});
-      return res.status(200).send(deletedHotel);
-    } catch (error) {
-      console.log("Failed to delete the Hotel: ", error);
-      res.status(500).send(error);
-    }
-  }); 
+router.delete("/:id", deleteHotel);
 
 //GET
-router.get("/:id", async (req, res) => {
-    try {
-      const hotel = await Hotel.findById(req.params.id);
-      if(!hotel) return res.status(404).send({message: "Hotel Not Found!"});
-      return res.status(200).send(hotel);
-    } catch (error) {
-      console.log("Failed to Find the Hotel: ", error);
-      res.status(500).send(error);
-    }
-  }); 
+router.get("/:id", getHotel);
 
 //GET ALL
-router.get("/", async (req, res, next) => {
-    try {
-      const hotel = await Hotel.find();
-      if(!hotel) return res.status(404).send({message: "Hotel Not Found!"});
-      return res.status(200).send(hotel);
-    } catch (error) {
-      next(error);
-    }
-  }); 
+router.get("/", getHotels);
 
 export default router;
